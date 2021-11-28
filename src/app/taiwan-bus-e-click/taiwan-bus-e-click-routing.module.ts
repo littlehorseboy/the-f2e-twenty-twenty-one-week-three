8aec05b67dc46048bc5396e0c37a9e474875769f
littlehorseboy <littlehorseboy@gmail.com>
2021-11-28 20:40:23 +0800

    feat(): header、footer

--- src/app/taiwan-bus-e-click/taiwan-bus-e-click-routing.module.ts
+++ src/app/taiwan-bus-e-click/taiwan-bus-e-click-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 import { TaiwanBusEClickComponent } from './taiwan-bus-e-click.component';
 
-const routes: Routes = [{ path: '', component: TaiwanBusEClickComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: TaiwanBusEClickComponent,
+    children: [
+      {
+        path: '',
+        component: IndexComponent,
+      },
+    ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/taiwan-bus-e-click/taiwan-bus-e-click.module.ts
+++ src/app/taiwan-bus-e-click/taiwan-bus-e-click.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
 import { TaiwanBusEClickRoutingModule } from './taiwan-bus-e-click-routing.module';
 import { TaiwanBusEClickComponent } from './taiwan-bus-e-click.component';
+import { IndexComponent } from './index/index.component';
 
 
 @NgModule({
   declarations: [
-    TaiwanBusEClickComponent
+    TaiwanBusEClickComponent,
+    IndexComponent
   ],
   imports: [
     CommonModule,
-    TaiwanBusEClickRoutingModule
+    TaiwanBusEClickRoutingModule,
+    ButtonModule,
+    ToolbarModule,
   ]
 })
 export class TaiwanBusEClickModule { }
