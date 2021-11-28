d20d438504605d62ff2abb8e7d5e8d1b33c7346e
littlehorseboy <littlehorseboy@gmail.com>
2021-11-28 22:24:11 +0800

    feat(): 第一頁，title 區塊、buses 區塊，CardModule

--- src/app/taiwan-bus-e-click/taiwan-bus-e-click.module.ts
+++ src/app/taiwan-bus-e-click/taiwan-bus-e-click.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
 import { TaiwanBusEClickRoutingModule } from './taiwan-bus-e-click-routing.module';
 import { TaiwanBusEClickComponent } from './taiwan-bus-e-click.component';
 import { IndexComponent } from './index/index.component';
-
 
 @NgModule({
   declarations: [
@@ -16,6 +16,7 @@ import { IndexComponent } from './index/index.component';
     CommonModule,
     TaiwanBusEClickRoutingModule,
     ButtonModule,
+    CardModule,
     ToolbarModule,
   ]
 })
